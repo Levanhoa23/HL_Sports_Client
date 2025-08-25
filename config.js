7986505ef5ebaa03a57168fa8031c6f184b8c05f
Hoa (John) V. LE <john@example.com>
2025-08-25 16:37:41 +0700

    feat(client): git push the firt

--- config.js
+++ config.js
@@ -1,23 +1,5 @@
-export const serverUrl = import.meta.env.VITE_BACKEND_URL;
+// config.js
 
-const checkConfig = (server) => {
-  let config = {};
-  switch (server) {
-    case "production":
-      config = {
-        baseUrl: "http://localhost:8000",
-      };
-      break;
-    case "local":
-      config = {
-        baseUrl: "http://localhost:8000",
-      };
-      break;
-    default:
-      break;
-  }
-  return config;
+export const config = {
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
 };
-
-export const selectServer = "local";
-export const config = checkConfig(selectServer);
